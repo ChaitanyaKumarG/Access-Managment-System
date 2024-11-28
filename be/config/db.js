c0bfea896fec48343a72973de5b89eb7193dc637
G Chaithanya Kumar <20551a0220.chaitanya@gmail.com>
2024-11-28 11:25:01 +0530

    removed config login credentials

--- be/config/db.js
+++ be/config/db.js
@@ -3,7 +3,7 @@ const mysql2 = require('mysql2')
 const db = mysql2.createConnection({
   host: "localhost",
   user: "root",
-  password: "ChaiLa#1321",
+  password: "",
   database: "user_management",
   dateStrings: true
 });
